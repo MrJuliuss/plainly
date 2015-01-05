597784090d5d84f873ffa140fd1f0c357ee639ff
Julien Richarte <julien.richarte@gmail.com>
2015-01-05 12:59:58 +0100

    Add social buttons

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(document).on('click', '.popup', function(event) {
+        var width  = 575,
+            height = 400,
+            left   = ($(window).width()  - width)  / 2,
+            top    = ($(window).height() - height) / 2,
+            url    = this.href,
+            opts   = 'status=1' +
+                     ',width='  + width  +
+                     ',height=' + height +
+                     ',top='    + top    +
+                     ',left='   + left;
+
+        window.open(url, '_blank', opts);
+
+        return false;
+    });
+});
